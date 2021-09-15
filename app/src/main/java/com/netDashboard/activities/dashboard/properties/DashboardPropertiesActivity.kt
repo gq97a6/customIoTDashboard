4f623a26026add2ad2e5d9f09c8addf431ab79ce
gq97a6 <szymon.kogut@protonmail.com>
2021-09-15 08:39:38 +0200

    Popup test.

--- app/src/main/java/com/netDashboard/activities/dashboard/properties/DashboardPropertiesActivity.kt
+++ app/src/main/java/com/netDashboard/activities/dashboard/properties/DashboardPropertiesActivity.kt
@@ -1,5 +1,6 @@
 package com.netDashboard.activities.dashboard.properties
 
+import android.app.Dialog
 import android.content.Intent
 import android.os.Bundle
 import android.text.Editable
@@ -9,6 +10,7 @@ import android.view.View.VISIBLE
 import android.view.animation.AccelerateDecelerateInterpolator
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
+import com.netDashboard.R
 import com.netDashboard.activities.MainActivity
 import com.netDashboard.activities.dashboard.DashboardActivity
 import com.netDashboard.activities.theme.ThemeActivity
@@ -17,6 +19,7 @@ import com.netDashboard.blink
 import com.netDashboard.dashboard.Dashboard
 import com.netDashboard.dashboard.Dashboard.Companion.byId
 import com.netDashboard.databinding.ActivityDashboardPropertiesBinding
+import com.netDashboard.databinding.PopupCopyBrokerBinding
 import com.netDashboard.globals.G
 import com.netDashboard.globals.G.dashboards
 import java.util.*
@@ -179,6 +182,16 @@ class DashboardPropertiesActivity : AppCompatActivity() {
         b.dpThemeUseOver.setOnCheckedChangeListener { _, state ->
             dashboard.theme.useOver = state
             theme.apply(this, b.root)
+        }
+
+        b.dpMqttCopy.setOnClickListener {
+            val dialog = Dialog(this)
+
+            dialog.setContentView(R.layout.popup_copy_broker)
+            val binding = PopupCopyBrokerBinding.bind(dialog.findViewById(R.id.cb_root))
+            theme.apply(this, binding.root)
+
+            dialog.show()
         }
     }
 
