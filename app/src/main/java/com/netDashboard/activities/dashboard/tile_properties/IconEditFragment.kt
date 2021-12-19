0f5e2c099c0faf13f9b763c2affd9e78c47e9808
gq97a6 <szymon.kogut@protonmail.com>
2021-12-19 07:33:36 +0100

    Revert.

--- app/src/main/java/com/netDashboard/activities/dashboard/tile_properties/IconEditFragment.kt
+++
@@ -1,7 +0,0 @@
-package com.netDashboard.activities.dashboard.tile_properties
-
-import androidx.fragment.app.Fragment
-import com.netDashboard.R
-
-class IconEditFragment : Fragment(R.layout.fragment_icon_edit) {
-}
--- app/src/main/java/com/netDashboard/activities/dashboard/tile_properties/TilePropertiesActivity.kt
+++ app/src/main/java/com/netDashboard/activities/dashboard/tile_properties/TilePropertiesActivity.kt
@@ -9,14 +9,12 @@ import android.view.View.GONE
 import android.view.View.VISIBLE
 import android.widget.RadioGroup
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.isVisible
 import com.netDashboard.R
 import com.netDashboard.activities.dashboard.DashboardActivity
 import com.netDashboard.app_on.AppOn
 import com.netDashboard.dashboard.Dashboard
 import com.netDashboard.dashboard.Dashboard.Companion.byId
 import com.netDashboard.databinding.ActivityTilePropertiesBinding
-import com.netDashboard.databinding.FragmentIconEditBinding
 import com.netDashboard.digitsOnly
 import com.netDashboard.globals.G
 import com.netDashboard.globals.G.dashboards
@@ -209,12 +207,6 @@ class TilePropertiesActivity : AppCompatActivity() {
                 (tile as SliderTile).dragCon = state
             }
         }
-
-        b.tpEditIcon.setOnClickListener {
-            b.tpFragment.let {
-                it.visibility = if(it.isVisible) GONE else VISIBLE
-            }
-        }
     }
 
     override fun onPause() {
@@ -289,12 +281,6 @@ class TilePropertiesActivity : AppCompatActivity() {
                     } else R.id.tp_mqtt_payload_val
                 )
             }
-        }
-
-        val frag = IconEditFragment()
-        supportFragmentManager.beginTransaction().apply {
-            replace(R.id.tp_fragment, frag)
-            commit()
         }
     }
 
