55b7a7c1f81eca1cf48380dbc7b1aca41eb21719
gq97a6 <szymon.kogut@protonmail.com>
2022-02-22 01:54:08 +0100

    TerminalTile implemented.

--- app/src/main/java/com/netDashboard/activities/fragments/DashboardNewFragment.kt
+++
@@ -1,6 +0,0 @@
-package com.netDashboard.activities.fragments
-
-import androidx.fragment.app.Fragment
-import com.netDashboard.R
-
-class DashboardNewFragment : Fragment(R.layout.fragment_dashboard_new)
