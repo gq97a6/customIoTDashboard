6586a7b9132f5a0f7a16c07ecf005da9335b0d73
gq97a6 <szymon.kogut@protonmail.com>
2024-07-05 04:47:29 +0200

    LaunchMode for activities specified. Application class added.

---
+++ android/src/main/java/com/alteratom/dashboard/AtomApp.kt
@@ -0,0 +1,11 @@
+package com.alteratom.dashboard
+
+import android.app.Application
+import com.alteratom.dashboard.objects.Setup
+
+
+class AtomApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
