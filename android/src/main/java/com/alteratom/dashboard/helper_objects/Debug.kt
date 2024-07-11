1e6e3c4966326e212ec14a4a664e3b51c7daedb2
gq97a6 <szymon.kogut@protonmail.com>
2024-07-11 12:58:18 +0200

    Custom remote logging added.

--- android/src/main/java/com/alteratom/dashboard/helper_objects/Debug.kt
+++ android/src/main/java/com/alteratom/dashboard/helper_objects/Debug.kt
@@ -1,13 +1,42 @@
 package com.alteratom.dashboard.helper_objects
 
 import io.sentry.Sentry
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import okhttp3.OkHttpClient
+import okhttp3.OkHttpClient.Builder
+import okhttp3.Request
+import okhttp3.RequestBody.Companion.toRequestBody
+import java.util.concurrent.TimeUnit
+
 
 object Debug {
+    private val client: OkHttpClient = Builder()
+        .connectTimeout(1, TimeUnit.SECONDS)
+        .build()
+
+    //private val client: OkHttpClient = OkHttpClient()
+    private val scope = CoroutineScope(Dispatchers.IO)
+
     fun log(payload: String) {
+        scope.launch { logRemote(payload) }
         Sentry.addBreadcrumb(payload)
     }
 
     fun recordException(exception: Exception) {
         Sentry.captureException(exception)
+    }
+
+    private fun logRemote(payload: String) {
+        try {
+            val request = Request.Builder()
+                .url("https://logger.hostunit.net/3958935417")
+                .post(payload.toRequestBody())
+                .build()
+
+            client.newCall(request).execute()
+        } catch (e: Exception) {
+        }
     }
 }
