edb761a2e3d4bd9c019010ba0acd6bfbab1fa712
gq97a6 <szymon.kogut@protonmail.com>
2024-07-27 03:40:45 +0200

    Fixed tile new fragment user interface.

--- android/src/main/java/com/alteratom/dashboard/fragment/TileNewFragment.kt
+++ android/src/main/java/com/alteratom/dashboard/fragment/TileNewFragment.kt
@@ -18,27 +18,25 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.GridItemSpan
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment.Companion.Center
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.fragment.app.Fragment
@@ -47,19 +45,96 @@ import com.alteratom.dashboard.Theme.Companion.colors
 import com.alteratom.dashboard.activity.MainActivity.Companion.fm
 import com.alteratom.dashboard.app.AtomApp.Companion.aps
 import com.alteratom.dashboard.compose_global.composeConstruct
-import com.alteratom.dashboard.tile.Tile
 
 class TileNewFragment : Fragment() {
+
+    val tileTags = listOf(
+        "Button",
+        "Slider",
+        "Switch",
+        "Text",
+        "Select",
+        "Terminal",
+        "Color picker",
+        "Time",
+        "Lights",
+        "Thermostat"
+    )
+
+    val tileIcons = listOf(
+        R.drawable.il_arrow_arrow_to_bottom,
+        R.drawable.il_arrow_arrows_h_alt,
+        R.drawable.il_interface_toggle_on,
+        R.drawable.il_design_illustration,
+        R.drawable.il_business_receipt_alt,
+        R.drawable.il_device_desktop,
+        R.drawable.il_design_palette,
+        R.drawable.il_time_clock,
+        R.drawable.il_business_lightbulb_alt,
+        R.drawable.il_weather_temperature_half
+    )
+
+    val tiles = listOf(
+        { ButtonTile() },
+        { SliderTile() },
+        { SwitchTile() },
+        { TextTile() },
+        { SelectTile() },
+        { TerminalTile() },
+        { ColorTile() },
+        { TimeTile() },
+        { LightsTile() },
+        { ThermostatTile() }
+    )
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View = composeConstruct(requireContext()) {
+
+        @Composable
+        fun TilePick(index: Int, aspectRatio: Float) {
+            Column(
+                modifier = Modifier
+                    .aspectRatio(aspectRatio)
+                    .clip(RoundedCornerShape(10.dp))
+                    .clickable {
+                        val tile = tiles[index]()
+                        tile.dashboard = aps.dashboard
+                        tile.onCreateTile()
+                        aps.dashboard.tiles.add(tile)
+
+                        aps.tile = tile
+                        fm.replaceWith(TilePropertiesFragment(), false)
+                    }
+                    .border(
+                        BorderStroke(1.dp, colors.color),
+                        RoundedCornerShape(10.dp)
+                    ),
+                horizontalAlignment = CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Icon(
+                    painterResource(tileIcons[index]),
+                    "",
+                    tint = colors.a,
+                    modifier = Modifier.size(40.dp)
+                )
+
+                Spacer(modifier = Modifier.height(6.dp))
+
+                Text(
+                    tileTags[index],
+                    fontSize = 15.sp,
+                    color = colors.b
+                )
+            }
+        }
+
         Column(
-            Modifier.padding(bottom = 20.dp),
-            Arrangement.Center,
-            CenterHorizontally
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = CenterHorizontally
         ) {
             Text(
                 "Pick new tile",
@@ -75,249 +150,27 @@ class TileNewFragment : Fragment() {
                 modifier = Modifier.fillMaxWidth(.85f)
             )
 
-            @Composable
-            fun Frame(
-                modifier: Modifier,
-                type: String,
-                res: Int,
-                tile: Tile?
-            ) {
-                Column(
-                    modifier = modifier
-                        .clip(RoundedCornerShape(10.dp))
-                        .clickable(tile != null) {
-                            tile?.let {
-                                it.dashboard = aps.dashboard
-                                it.onCreateTile()
-                                aps.dashboard.tiles.add(it)
-
-                                aps.tile = it
-                                fm.replaceWith(TilePropertiesFragment(), false)
-                            }
-                        }
-                        .border(
-                            BorderStroke(1.dp, colors.color),
-                            RoundedCornerShape(10.dp)
-                        )
-                        .padding(10.dp)
-                        .padding(bottom = 5.dp),
-                    horizontalAlignment = CenterHorizontally
-                ) {
-                    Text(
-                        type,
-                        fontSize = 15.sp,
-                        color = colors.b
-                    )
-
-                    Icon(
-                        painterResource(res),
-                        "",
-                        tint = colors.a,
-                        modifier = Modifier
-                            .fillMaxHeight()
-                            .aspectRatio(1f)
-                            .padding(top = 7.dp)
-                    )
-                }
-            }
-
-            val padding = 10.dp
-            Column(
+            LazyVerticalGrid(
+                columns = GridCells.Adaptive(80.dp),
+                verticalArrangement = Arrangement.spacedBy(10.dp),
+                horizontalArrangement = Arrangement.spacedBy(10.dp),
                 modifier = Modifier
-                    .fillMaxWidth(.85f)
                     .padding(top = 20.dp)
-                    .border(
-                        BorderStroke(1.dp, colors.color),
-                        RoundedCornerShape(10.dp)
-                    )
-                    .padding(end = padding, bottom = padding, top = padding)
+                    .fillMaxWidth(.85f)
             ) {
-                Row {
-                    Frame(
-                        modifier = Modifier
-                            .padding(start = padding)
-                            .weight(1f)
-                            .aspectRatio(1f),
-                        "Button",
-                        R.drawable.il_arrow_arrow_to_bottom,
-                        ButtonTile()
+                items(10, span = { index ->
+                    GridItemSpan(if (index in listOf(6, 9)) 2 else 1)
+                }) { index ->
+                    TilePick(
+                        index, if (index in listOf(
+                                6, 9
+                            )
+                        ) 2f else 1f
                     )
-
-                    Frame(
-                        modifier = Modifier
-                            .padding(start = padding)
-                            .weight(1f)
-                            .aspectRatio(1f),
-                        "Slider",
-                        R.drawable.il_arrow_arrows_h_alt,
-                        SliderTile()
-                    )
-
-                    Frame(
-                        modifier = Modifier
-                            .padding(start = padding)
-                            .weight(1f)
-                            .aspectRatio(1f),
-                        "Switch",
-                        R.drawable.il_interface_toggle_on,
-                        SwitchTile()
-                    )
-                }
-
-                Row(modifier = Modifier.padding(top = padding)) {
-                    Frame(
-                        modifier = Modifier
-                            .padding(start = padding)
-                            .weight(1f)
-                            .aspectRatio(1f),
-                        "Text",
-                        R.drawable.il_design_illustration,
-                        TextTile()
-                    )
-
-                    Frame(
-                        modifier = Modifier
-                            .padding(start = padding)
-                            .weight(1f)
-                            .aspectRatio(1f),
-                        "Select",
-                        R.drawable.il_business_receipt_alt,
-                        SelectTile()
-                    )
-                    Frame(
-                        modifier = Modifier
-                            .padding(start = padding)
-                            .weight(1f)
-                            .aspectRatio(1f),
-                        "Terminal",
-                        R.drawable.il_device_desktop,
-                        TerminalTile()
-                    )
-                }
-
-                Row(modifier = Modifier.padding(top = padding)) {
-                    Frame(
-                        modifier = Modifier
-                            .padding(start = padding)
-                            .weight(1f)
-                            .aspectRatio(1.5f),
-                        "Color picker",
-                        R.drawable.il_design_palette,
-                        ColorTile()
-                    )
-
-                    Frame(
-                        modifier = Modifier
-                            .padding(start = padding)
-                            .weight(1f)
-                            .aspectRatio(1.5f),
-                        "Thermostat",
-                        R.drawable.il_weather_temperature_half,
-                        ThermostatTile()
-                    )
-                }
-
-                Row(modifier = Modifier.padding(top = padding)) {
-                    Frame(
-                        modifier = Modifier
-                            .padding(start = padding)
-                            .weight(1f)
-                            .aspectRatio(1f),
-                        "Time",
-                        R.drawable.il_time_clock,
-                        TimeTile()
-                    )
-
-                    Frame(
-                        modifier = Modifier
-                            .padding(start = padding)
-                            .weight(1f)
-                            .aspectRatio(1f),
-                        "Lights",
-                        R.drawable.il_business_lightbulb_alt,
-                        LightsTile()
-                    )
-
-                    Box(
-                        modifier = Modifier
-                            .padding(start = padding)
-                            .weight(1f)
-                            .aspectRatio(1f),
-                        contentAlignment = Center
-                    ) {
-                        Text(
-                            "COMING\nSOON",
-                            modifier = Modifier.rotate(10f),
-                            fontSize = 15.sp,
-                            fontWeight = FontWeight.Bold,
-                            textAlign = TextAlign.Center,
-                            color = colors.b
-                        )
-                        Frame(
-                            modifier = Modifier
-                                .fillMaxSize()
-                                .alpha(.3f),
-                            "Graph",
-                            R.drawable.il_business_analysis,
-                            null
-                        )
-                    }
-                }
-
-                Row(modifier = Modifier.padding(top = padding)) {
-                    Box(
-                        modifier = Modifier
-                            .padding(start = padding)
-                            .weight(1f)
-                            .aspectRatio(1.5f),
-                        contentAlignment = Center
-                    ) {
-                        Text(
-                            "COMING\nSOON",
-                            modifier = Modifier.rotate(350f),
-                            fontSize = 15.sp,
-                            fontWeight = FontWeight.Bold,
-                            textAlign = TextAlign.Center,
-                            color = colors.b
-                        )
-
-                        Frame(
-                            modifier = Modifier
-                                .fillMaxSize()
-                                .alpha(.3f),
-                            "Keyboard",
-                            R.drawable.il_interface_keyboard,
-                            null
-                        )
-                    }
-
-                    Box(
-                        modifier = Modifier
-                            .padding(start = padding)
-                            .weight(1f)
-                            .aspectRatio(1.5f),
-                        contentAlignment = Center
-                    ) {
-                        Text(
-                            "COMING\nSOON",
-                            modifier = Modifier.rotate(20f),
-                            fontSize = 15.sp,
-                            fontWeight = FontWeight.Bold,
-                            textAlign = TextAlign.Center,
-                            color = colors.b
-                        )
-
-                        Frame(
-                            modifier = Modifier
-                                .fillMaxSize()
-                                .alpha(.3f),
-                            "Touchpad",
-                            R.drawable.il_device_mouse_alt,
-                            null
-                        )
-                    }
                 }
             }
+
+            Spacer(modifier = Modifier.fillMaxHeight(.2f))
         }
     }
 }
